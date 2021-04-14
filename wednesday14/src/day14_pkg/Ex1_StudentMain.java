c3f68f68ad6b56adcae9cc9049cf3ff9325c65f0
user <user@DESKTOP-QQG1134>
2021-04-14 10:23:23 +0900

    메소드 추가내용

--- wednesday14/src/day14_pkg/Ex1_StudentMain.java
+++ wednesday14/src/day14_pkg/Ex1_StudentMain.java
@@ -16,10 +16,29 @@ public class Ex1_StudentMain {
 		System.out.println(stu2.name);
 		System.out.println(stu2.stuNum);
 		
+		//Student stu3 =new Student(2222,"학생3");
+		System.out.println("메소드 호출전");
 		
+		//fieldPrint 메소드 호출
+		stu1.fieldPrint();
+		System.out.println("메소드 호출끝");
 		
+		stu2.fieldPrint();
 		
+		Student stu3 =new Student("학생3",3333);
+		stu3.fieldPrint();
 		
+		System.out.println("리턴있는 메소드 호출");
+		//리턴이 있는 메소드를 호출할 때는 호출만하고 끝
+		//끝나는 것이 아니라 리턴값을 활용하는 내용이 필요
+		
+		stu1.numberMethod();
+		int aa=0;
+		System.out.println(aa);//메소드호출전
+		aa=stu1.numberMethod();
+		System.out.println(aa);//메소드호출후
+		
+		System.out.println(stu2.numberMethod());
 	}
 
 }
--- wednesday14/src/day14_pkg/Student.java
+++ wednesday14/src/day14_pkg/Student.java
@@ -10,11 +10,45 @@ public class Student {
 	//정의하면 기본생성자를 쓸 수 없음.
 	//기본생성자를 쓰고 싶다면 따로 정의를 해줘야함.
 	Student(){
-		
+		System.out.println("기본생성자 호출");
 	}
 	//매개변수가 있는 생성
 	Student(String name,int stuNum){
+		System.out.println("매개변수 있는 생성자 호출");
 		this.name=name;
 		this.stuNum=stuNum;
+	}	
+		
+	//메소드 선언
+	//리턴이 없는 메소드 선언
+		/*
+		 * void: 리턴이 없다는 의미
+		 * 메소드이름:fieldPrint
+		 * 매개변수:없음
+		 */
+	void fieldPrint() {
+		System.out.println("메소드 호출됨");
+		System.out.println(this.name);
+		System.out.println(this.stuNum);
 	}
-}
+	//리턴이 있는 메소드 선언
+	/*
+	 * int:리턴되는 값이 int
+	 * 메소드이름:numberMethod
+	 * 매개변수:없음
+	 */
+	int numberMethod() {
+		int num=10;//정수형 변수
+		return num;//num변수가 가진 값을 리턴해줌
+	}
+	
+	
+	
+	
+}	
+		
+	
+	
+
+	
+
